4a526297997a93fd8f114598e30aff5f07fee05f
stephen olise <stpholise@gmail.com>
2026-01-06 09:51:58 +0100

    signup controller added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,33 +1,33 @@
-
-import express from "express"
-import dotenv from "dotenv"
-import path from 'path';
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-
-
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 dotenv.config();
 
-const PORT = process.env.PORT  || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use("/api/auth",  authRoutes)
+app.use(express.json());
 
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
 
+app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get(/.*/, (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend/dist/index.html")
-    );
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
-app.listen(3000, () => console.log("server running on port 3000 this"))
+app.listen(PORT, () => {
+  console.log("server running on port " + PORT);
+  connectDB();
+});
