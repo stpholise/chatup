3d924bde467c0ed7176530fd664682197557f9de
stephen olise <stpholise@gmail.com>
2026-01-10 16:29:34 +0100

    tests for backend deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,13 +20,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+//   app.get(/.*/, (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+//   });
+// }
+
+app.get("/", (req, res) => {
+  res.json({ status: "Backend is live 🚀" });
+});
+
 
 app.listen(PORT, () => {
   console.log("server running on port " + PORT);
